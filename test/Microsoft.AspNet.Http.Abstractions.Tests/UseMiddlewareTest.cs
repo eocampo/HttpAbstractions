1f6c1d5f589134904dbd9190f9a1fb317f3d36a4
Mikael Mengistu <mikaelm@mit.edu>
2015-06-19 18:04:00 -0700

    Formmating

--- test/Microsoft.AspNet.Http.Abstractions.Tests/UseMiddlewareTest.cs
+++ test/Microsoft.AspNet.Http.Abstractions.Tests/UseMiddlewareTest.cs
@@ -1,13 +1,11 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System.IO;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Http.Internal;
-using Xunit;
-using Microsoft.AspNet.Builder.Internal;
 using System;
+using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Builder.Internal;
+using Xunit;
 
 namespace Microsoft.AspNet.Http
 {
@@ -20,35 +18,27 @@ namespace Microsoft.AspNet.Http
             var builder = new ApplicationBuilder(mockServiceProvider);
             builder.UseMiddleware(typeof(MiddlewareNoParameters));
             var exception = Assert.Throws<InvalidOperationException>(() => builder.Build());
-            Assert.Equal("Middleware Invoke method must take first argument of HttpContext", exception.Message);
-           // builder.Build();
-           
-           
+            Assert.Equal("Middleware Invoke method must take first argument of HttpContext", exception.Message); 
         }
 
         [Fact]
         public void UseMiddlewareNonTaskReturnType()
         {
-
             var mockServiceProvider = new MockServiceProvider();
             var builder = new ApplicationBuilder(mockServiceProvider);
             builder.UseMiddleware(typeof(MiddlewareNonTaskReturn));
             var exception = Assert.Throws<InvalidOperationException>(() => builder.Build());
             Assert.Equal("Invoke does not return an object of type Task", exception.Message);
-        //    Console.WriteLine(builder.UseMiddleware(typeof(MiddlewareNonTaskReturn)));
         }
 
         [Fact]
         public void UseMiddlewareInvokeMissing()
-        {
-            
+        {          
             var mockServiceProvider = new MockServiceProvider();
             var builder = new ApplicationBuilder(mockServiceProvider);
             builder.UseMiddleware(typeof(MiddlewareNoInvoke));
             var exception = Assert.Throws<InvalidOperationException>(() => builder.Build());
             Assert.Equal("No public Invoke method found", exception.Message);
-
-
         }
 
         private class MockServiceProvider : IServiceProvider
@@ -63,8 +53,8 @@ namespace Microsoft.AspNet.Http
         {
             public MiddlewareNoParameters(RequestDelegate next)
             {
-
             }
+
             public Task Invoke()
             {
                 return Task.FromResult(0);
@@ -75,8 +65,8 @@ namespace Microsoft.AspNet.Http
         {
             public MiddlewareNonTaskReturn(RequestDelegate next)
             {
-
             }
+
             public int Invoke()
             {
                 return 0;
@@ -87,9 +77,7 @@ namespace Microsoft.AspNet.Http
         {
             public MiddlewareNoInvoke(RequestDelegate next)
             {
-
             }
-
         }
     }
 }
